ab14c17d94779cbf343840f2e0eee227fca19b3c
Arturo K <arturo@bookshout.com>
2016-04-05 18:47:36 +0000

    Also index.

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ sed -i "s;^# elasticsearch.url:.*;elasticsearch.url: ${KIBANA_ES_URL};" "${KIBAN
 
 if [ -n "${KIBANA_INDEX}" ]; then
     echo "setting index!"
-    sed -i "s;^kibana.index:.*;kibana.index: ${KIBANA_INDEX};" "${KIBANA_CONF_FILE}"
+    sed -i "s;^# kibana.index:.*;kibana.index: ${KIBANA_INDEX};" "${KIBANA_CONF_FILE}"
 fi
 
 # mesos-friendly change
